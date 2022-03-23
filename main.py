32c5f25d901d21ad93dacb7622d0d0fd5fd888af
Gabriel Bitti <gabrielbitti@Gabriels-MacBook-Pro.local>
2022-03-23 01:30:27 -0300

    Ajusta rota para retornar temperatura certa de uma cidade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import requests
+import json
 # TODO import Weather here!!!!
 
 
@@ -9,6 +10,7 @@ Call current weather data
 API KEY: b77e07f479efe92156376a8b07640ced
 https://api.openweathermap.org/data/2.5
 """
+
 
 class Weather():
     config = {
@@ -20,10 +22,14 @@ class Weather():
         self.city_name = city
         self.latitude = latitude
         self.longitude = longitude
-    
+
     def current_temperature(self):
-        request_result = requests.get("{url}/weather?lat={lat}&lon={lon}&appid={api_key}".format(url=self.config['base_url'], lat=self.latitude, lon=self.longitude, api_key=self.config['api_key']))
-        return request_result.content
+        request_result = requests.get("{url}/weather?q={city}&appid={api_key}".format(
+            url=self.config['base_url'], city=self.city_name, api_key=self.config['api_key']))
+        request_content = request_result.content
+        #request_content.temperature_in_celcius = request_content.main.temp #- 237.15
+
+        return request_content
 
 
 app = FastAPI()
@@ -36,5 +42,13 @@ def root():
 
 @app.get("/cities/{name}")
 def read_city(name: str):
-    weather = Weather(city=name, latitude='lattt', longitude='long')
-    return {"city_name": weather.current_temperature()}
+    weather = Weather(city=name, latitude='', longitude='')
+    current_temperature = weather.current_temperature()
+    tempereture = current_temperature
+
+    my_json = current_temperature.decode('utf8').replace("'", '"')
+    data = json.loads(my_json)
+    # s = json.dumps(data, indent=4, sort_keys=True)
+    temperature_in_celcius = int(data['main']['temp']) - 237
+
+    return {'temperature': temperature_in_celcius}
