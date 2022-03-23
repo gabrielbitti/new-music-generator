1c7acaebb8ac3c35ecef9351e61d810edf5cb231
Gabriel Bitti <gabrielbitti@Gabriels-MacBook-Pro.local>
2022-03-22 23:31:56 -0300

    Cria base de scripts para requisitar informações do clima de uma cidade

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-from typing import Optional
 from fastapi import FastAPI
+import requests
+# import Weather here!!!!
+
+
+"""
+Call current weather data
+
+API KEY: b77e07f479efe92156376a8b07640ced
+https://api.openweathermap.org/data/2.5
+"""
+
+class Weather():
+    config = {
+        'api_key': 'b77e07f479efe92156376a8b07640ced',
+        'base_url': 'https://api.openweathermap.org/data/2.5',
+    }
+
+    def __init__(self, city, latitude, longitude):
+        self.city_name = city
+        self.latitude = latitude
+        self.longitude = longitude
+    
+    def current_temperature(self):
+        request_result = requests.get("{url}/weather?lat={lat}&lon={lon}&appid={api_key}".format(url=self.config['base_url'], lat=self.latitude, lon=self.longitude, api_key=self.config['api_key']))
+        return request_result.content
+
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    return "New Playlist Generator"
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/cities/{name}")
+def read_city(name: str):
+    weather = Weather(city=name, latitude='lattt', longitude='long')
+    # TODO get the tempereture of this city
+    return {"city_name": weather.current_temperature()}
